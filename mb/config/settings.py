0532461c934ddf0244d79e75f78be5f8af2f81d5
Ayodeji Yekeen <ayodejiyou@gmail.com>
2023-04-04 15:01:25 +0000

    add messages

--- mb/config/settings.py
+++ mb/config/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [str(BASE_DIR.joinpath("templates"))],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://deji01-crispy-space-yodel-v59w69p4qr9fxw96-8000.preview.app.github.dev"
+]
